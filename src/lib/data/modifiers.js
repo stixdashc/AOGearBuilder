b7a9f7814e1f8e8b4749dbb239a7b1a7e49372fe
stixdashc <150515888+stixdashc@users.noreply.github.com>
2023-11-11 01:46:37 -0800

    Update modifiers.js

--- src/lib/data/modifiers.js
+++ src/lib/data/modifiers.js
@@ -45,6 +45,28 @@ const modifiersData = [
 		rarityColor: '#ED120E',
 		imageId: 'https://i.imgur.com/JvqDmI1.jpg'
 	}
+	{
+		id: 1,
+		name: 'Drowned Modifier',
+		legend:
+			'Gives stats per 10 levels rounded down in the following order depending on if your item has the stat or not: Power -> Defense -> Attack Size -> Attack Speed -> Agility -> Intensity -> Power',
+		defense: 4.41666666667, // Defense Changed?
+		power: 0,
+		agility: 0,
+		attackSize: 1.5,
+		attackSpeed: 0,
+		intensity: 0,
+		insanity: 0,
+		drawback: 0,
+		warding: 0,
+		maxLevel: 0,
+		gemNo: 0,
+		mainType: 'Modifier',
+		subType: 'None',
+		rarity: 'Exotic',
+		rarityColor: '#ED120E',
+		imageId: 'https://i.imgur.com/JvqDmI1.jpg'
+	}
 ];
 
 export const modifiers = getData(modifiersData);
